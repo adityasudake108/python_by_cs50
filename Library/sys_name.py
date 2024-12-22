4cb4bd818a83b88e4304220d0140033771d1915e
Hunter Adi <hunteradi727@gmail.com>
2024-12-22 15:34:43 +0530

    sys_name.py: Add a new function to get the system

--- Library/sys_name.py
+++ Library/sys_name.py
@@ -6,13 +6,14 @@ import sys
 # except IndexError:
 #     print("Please provide a name")
 
+'''
 if len(sys.argv) < 2:                           ### Method 2
     sys.exit("Please provide a name")
 elif len(sys.argv) > 2:
     sys.exit("You provided too many names")
 else:
     print("Hello, my name is", sys.argv[1])
-
+'''
 
 # if you want to print the name of the script, you can use sys.argv[0] as well.
 # you want to print the name like David John, you should insert between " ".
@@ -20,3 +21,8 @@ else:
 # sys.exit("Please provide a name")   This will exit the program and print the message.
 
 
+if len(sys.argv) < 2:                           ### Method 2
+    sys.exit("Please provide a name")
+
+for arg in sys.argv[1:]:
+    print("Hello, my name is", arg)
