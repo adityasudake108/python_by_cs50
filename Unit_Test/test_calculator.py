401a1779324b587d22f0e51fee7d375e163a25f9
Hunter Adi <hunteradi727@gmail.com>
2024-12-30 21:18:56 +0530

    In this we use pytest framework for to check test cases using try catch block

--- Unit_Test/test_calculator.py
+++ Unit_Test/test_calculator.py
@@ -1,33 +1,47 @@
 from calculator import square
 
-def main():
-    test_square()
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
 
-def test_square():
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
 
-    # Method 1
-    # if square(2) !=4:
-    #     print("2 squared was not 4")
-    # if square(3) !=9:
-    #     print("3 squared was not 9")
-    # else:
-    #     print("All tests passed")
+def test_zero():
+    assert square(0) == 0
 
 
-    # Proper Method using "assert" key
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 squared was not 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 squared was not 9")
+
+
+# def main():
+#     test_square()
+
+# def test_square():
+
+#     # Method 1
+#     # if square(2) !=4:
+#     #     print("2 squared was not 4")
+#     # if square(3) !=9:
+#     #     print("3 squared was not 9")
+#     # else:
+#     #     print("All tests passed")
+
+
+#     # Proper Method using "assert" key
+#     try:
+#         assert square(2) == 4
+#     except AssertionError:
+#         print("2 squared was not 4")
+#     try:
+#         assert square(3) == 9
+#     except AssertionError:
+#         print("3 squared was not 9")
         
     
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
 
 # The assert statement is used to check if a condition is True. If the condition is False, the program will raise an AssertionError.
