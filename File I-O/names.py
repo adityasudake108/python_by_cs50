ec4b6ea12f689a93f110f174aa0dc254cf5bac5a
adityasudake108 <adityasudake108@gmail.com>
2025-01-13 17:59:48 +0530

    In this we use file I/O opertions to read and write the data in the file. Implement File I/O operations in names.py and names.txt
    - Added names.py to demonstrate reading from and writing to a text file.
    - Created names.txt to store sample data for the I/O operations.

---
+++ File I-O/names.py
@@ -0,0 +1,26 @@
+# name = input("What is your name? ")
+
+
+# Open file in write mode
+
+# file = open("names.txt", "a")
+# file.write(f"{name}\n")
+# file.close()
+
+# Using "with" statement to open file in write mode and close it automatically
+
+# with open("names.txt", "a") as file:
+#     file.write(f"{name}\n")
+
+# Open file in read mode
+
+# with open("names.txt", "r") as file:
+#     for line in file:
+#         print("hello,", line.strip())
+
+names = []
+
+with open("names.txt") as file:
+    for line in sorted(file, reverse=True):
+        print(line.rstrip())
+
