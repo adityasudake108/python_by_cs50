eb9c141ed3f0fafd4d64e0dfec911ac9574ed798
adityasudake108 <adityasudake108@gmail.com>
2025-01-15 20:22:48 +0530

    Read, sort, and print student names and houses from students.csv
    - Read each line from students.csv
    - Strip trailing whitespace and split by comma to get name and house
    - Store each student in a dictionary and append to a list
    - Define a function to get the name from a student dictionary
    - Sort the list of students by names
    - Print each student's name and house in a formatted string

--- File I-O/students.py
+++ File I-O/students.py
@@ -7,16 +7,34 @@
         
 
 
-#code 2
+#Code 2
 
-students = []
+# students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        students.append(f"{name} is in {house}")
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         students.append(f"{name} is in {house}")
 
-for student in sorted(students):
-    print(student)
+# for student in sorted(students):
+#     print(student)
 
 #Above code reads student names and houses from students.csv, formats them into strings, sorts the list of formatted strings, and prints each sorted entry.
+
+#Code 3
+
+students = []
+with open ("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house":house}
+        students.append(student)
+
+def get_name(student):
+    return student["name"]
+
+for student in sorted(students, key=get_name):
+    print(f"{student['name']} is in {student['house']}")  
+
+# This code reads student names and houses from students.csv, stores them in a list of dictionaries,
+# sorts the list by student names, and prints each student's name and house.
